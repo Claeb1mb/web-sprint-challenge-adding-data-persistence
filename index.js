f06c6c2898ffa5b3337695f2ab98638cd2196c91
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2021-02-06 21:23:36 -0500

    server listen set up index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server')
+
+const Port = process.env.Port || 4000;
+
+server.listen(Port, () => {
+    console.log(`Api Running On Port: ${Port}`)
+})
